899f4d6e335efd8b9996fff719fa5a8f1f075e0c
ambud <asharma525xi@gmail.com>
2017-03-13 17:11:08 -0700

    Adding log lines

--- src/main/java/com/srotya/collectd/ambari/AmbariMetrics.java
+++ src/main/java/com/srotya/collectd/ambari/AmbariMetrics.java
@@ -86,12 +86,14 @@ public class AmbariMetrics implements CollectdConfigInterface, CollectdReadInter
 			baseUrl = baseUrl.replace("{{start_time}}", String.valueOf(ts));
 			baseUrl = baseUrl.replace("{{end_time}}", String.valueOf(System.currentTimeMillis()));
 
+			Collectd.logDebug("Ambari metrics base url:" + baseUrl);
+
 			HttpGet request = new HttpGet(baseUrl);
 			CloseableHttpClient client = builder.build();
 			try {
 				CloseableHttpResponse response = client.execute(request);
 				String result = EntityUtils.toString(response.getEntity());
-
+				Collectd.logDebug("Ambari metrics result:" + result);
 				JsonObject object = new Gson().fromJson(result, JsonObject.class);
 				for (JsonElement element : object.get("metrics").getAsJsonArray()) {
 					JsonObject metric = element.getAsJsonObject();
@@ -179,6 +181,7 @@ public class AmbariMetrics implements CollectdConfigInterface, CollectdReadInter
 		}
 		ts = System.currentTimeMillis() - 60_000;
 		Collectd.logInfo("Configured Ambari Metrics Collectd Plugin with:" + instance.toString());
+		Collectd.logInfo("Plugin will capture metrics for:" + appMetrics);
 		return 0;
 	}
 
